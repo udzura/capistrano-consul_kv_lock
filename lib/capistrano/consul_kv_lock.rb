d36147a93bc1b20658526c2c638511a2106ee41d
Uchio KONDO <udzura@pepabo.com>
2015-04-17 13:58:16 +0900

    Introduce session control

--- lib/capistrano/consul_kv_lock.rb
+++ lib/capistrano/consul_kv_lock.rb
@@ -1,10 +1,5 @@
-require 'capistrano/framework'
-load File.expand_path("../tasks/consul_kv_lock.rake", __FILE__)
-
-module Capistrano
-  module ConsulKvLock
-    # Your code goes here...
-  end
-end
-
+require "capistrano/framework"
+require "capistrano/consul_kv_lock/latch"
 require "capistrano/consul_kv_lock/version"
+
+load File.expand_path("../tasks/consul_kv_lock.rake", __FILE__)
---
+++ lib/capistrano/consul_kv_lock/latch.rb
@@ -0,0 +1,85 @@
+module Capistrano
+  module ConsulKvLock
+    class SSHKittyLogger
+      def initialize
+        @coordinator = SSHKit::Coordinator.new('localhost')
+      end
+
+      %w(debug info warn error fatal).each do |ll|
+        define_method(ll) do |str|
+          @coordinator.each do
+            run_locally { self.send(ll, "[consul-client] #{str}") }
+          end
+        end
+      end
+    end
+
+    class Latch
+      def self.set_instance(consul_url, options={})
+        @_instance ||= p(new(consul_url, options))
+      end
+
+      def self.instance
+        @_instance
+      end
+
+      def initialize(consul_url, options={})
+        @logger = SSHKittyLogger.new
+        @client = begin
+                   consul = URI.parse(consul_url)
+                   Consul::Client.v1.http(host: consul.host, port: consul.port, logger: @logger)
+                  end
+        @lock_key = options[:consul_lock_key] || 'deployment/locked'
+        @session_id = nil
+      end
+      attr_reader :client, :lock_key
+
+      def session_request
+        {
+          "LockDelay" => 15,
+          "Name" => "lock-for-#{lock_key.tr("/", "-")}",
+        }
+      end
+
+      def locked?
+        r = client.get("/kv/#{lock_key}")
+        !!(Base64.decode64(r[0]['Value']) =~ /\A["'](t(rue)?|1|y(es)?)["']\z/)
+      rescue Consul::Client::ResponseException => e
+        # in case of 404
+        if e.message.include?('404')
+          return false
+        else
+          raise e
+        end
+      end
+
+      def create_session
+        @logger.debug "Session request: #{session_request.inspect}"
+        r = client.put("/session/create", session_request)
+        @session_id = r['ID']
+      end
+
+      def delete_session
+        with_session {
+          client.put("/session/destroy/#{@session_id}", "")
+        }
+        @session_id = nil
+      end
+
+      def lock
+        with_session { client.put("/kv/#{lock_key}?acquire=#{@session_id}", "true") }
+      end
+
+      def unlock
+        with_session { client.put("/kv/#{lock_key}?release=#{@session_id}", "false") }
+      end
+
+      private
+      def with_session &blk
+        return false unless @session_id
+        blk.call
+        return true
+      end
+    end
+  end
+end
